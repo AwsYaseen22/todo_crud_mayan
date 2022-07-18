95fd74cb5132096fdd63022556a8839e9fdd291f
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-07-18 22:42:14 +0300

    connect to the DB

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.DB_CONNECTION, () =>
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
   console.log("connected to DB")
 );
 
