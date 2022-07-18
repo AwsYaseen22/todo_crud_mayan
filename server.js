811be80cff1004779cab8a8f801fb1e803305004
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-07-19 00:06:07 +0300

    read data from db and post new task to db

--- server.js
+++ server.js
@@ -3,12 +3,36 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const mongoose = require("mongoose");
+const TodoTask = require("./model/todoTask");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("test"));
+app.get("/", async (req, res) => {
+  try {
+    const tasks = await TodoTask.find({});
+    console.log(tasks);
+    res.render("index.ejs", { todoTasks: tasks });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post("/", async (req, res) => {
+  const { title, content } = req.body;
+  const todoTask = new TodoTask({
+    title,
+    content,
+  });
+  try {
+    await todoTask.save();
+    console.log(todoTask);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
   console.log("connected to DB");
